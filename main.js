f9d46ba1237e97e665fc9b4607080401edc57db7
Unknown <jakash1997@gmail.com>
2019-03-07 13:31:18 +0530

    Change styled thing

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let current = 'div'
-const codeBuilder = (name,style) => {
+const codeBuilder = (name,type,style) => {
     return `
     import styled from 'styled-components';<br/><br/>
 
-    const ${name} = styled.button\`<br/>
+    const ${name} = styled.${type}\`<br/>
         ${style}
     \`;<br/><br/>
 
@@ -15,6 +15,7 @@ const displayFunc = () => {
     document.querySelector('#'+current).style.display = 'none';
     current = document.querySelector('#mySelector').value
     document.querySelector('#'+ current).style = document.querySelector('#userCSS').value;
+    updateCSS()
 }
 
 const updateCSS = () => {
@@ -24,7 +25,7 @@ const updateCSS = () => {
     style = style.replace(/\r/g,'<br/>');
     style = style.replace(/\n/g,'<br/>');
     document.querySelector('#'+current).style = style;
-    document.querySelector('#outputCode').innerHTML = codeBuilder(name,style)
+    document.querySelector('#outputCode').innerHTML = codeBuilder(name,current,style)
 }
 
 
